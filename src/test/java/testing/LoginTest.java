eb8f3170efb0b12920975f5657da004ab4cc9325
Amul Sinfal <amul.sinfal@gmail.com>
2026-07-12 14:58:56 +0530

    priority_testng.xml

---
+++ src/test/java/testing/LoginTest.java
@@ -0,0 +1,33 @@
+package testing;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+public class LoginTest {
+
+	@Test(priority = 1, groups = { "Smoke", "Regression" }, 
+			description = "Test Login With Valid Credentials")
+	public void testLoginWithValidCredentials() {
+		Assert.assertTrue(true);
+		System.out.println("Inside testLoginWithValidCredentials");
+	}
+
+	@Test(priority = 2, groups = { "Regression" }, 
+			description = "Test Login With invalid Credentials")
+	public void testLoginWithInvalidCredentials() {
+		Assert.assertTrue(true);
+		System.out.println("Inside testLoginWithInvalidCredentials");
+	}
+
+	@Test(priority = 3, groups = { "Regression" }, 
+			description = "Test Login With blank Credentials")
+	public void testLoginWithBlankCredentials() {
+		Assert.assertTrue(true);
+		System.out.println("Inside testLoginWithBlankCredentials");
+	}
+
+	@Test(priority = 4, groups = { "Regression" }, 
+			description = "Test Login With locked Credentials")
+	public void testLoginWithLockedCredentials() {
+		Assert.assertTrue(true);
+		System.out.println("Inside testLoginWithLockedCredentials");
+	}
+}
