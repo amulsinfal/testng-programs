e98b2fa8c743aa961fc0570410e9f3189fe9fd3b
Amul Sinfal <amul.sinfal@gmail.com>
2026-07-17 10:14:19 +0530

    Add DataProviderExample.java file.

--- src/test/java/testing/DataProviderExample.java
+++ src/test/java/testing/DataProviderExample.java
@@ -7,8 +7,17 @@ import dataProviders.TestDataProvider;
 public class DataProviderExample {
 	
 	@Test(dataProvider = "loginData", dataProviderClass = TestDataProvider.class)
-	public void launchBrowser(String username, String password) {
+	public void doLogin(String username, String password) {
 		System.out.println("Username: " + username);
 		System.out.println("Password: " + password);
+	}
+	
+	@Test(dataProvider = "excelLoginData", dataProviderClass = TestDataProvider.class)
+	public void verifyMessageAfterLogin(String username, String password, String message) {
+		System.out.println("Username in the excel file: " + username);
+		System.out.println("Password in the excel file: " + password);
+		System.out.println("Message in the excel file: " + message);
+		System.out.println();
+		
 	}
 }
